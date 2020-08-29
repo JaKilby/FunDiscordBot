788cdc6bb8dd30e75ea3c77e75842f44a182ecdb
unknown <jacob@ajsmk.com>
2020-08-28 20:56:40 -0700

    Setting procfile to web

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
 import discord
+import os
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
