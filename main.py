37dbf32456d7665674872d3b9f08e4228a0ccba6
unknown <jacob@ajsmk.com>
2020-08-28 20:38:38 -0700

    Initial commit to set up on Heroku

---
+++ main.py
@@ -0,0 +1,49 @@
+# This is a sample Python script.
+import discord
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+class MyClient(discord.Client):
+    async def on_read(self):
+        print("Logged on as {}".format(self.user))
+
+    async def on_message(self, message):
+        print("Message from {0.author}: {0.content}".format(message))
+        #<:FishEmote: 742769011833438241 >
+        if message.author.name == "gmink":
+            await message.channel.send("shut up console nerd")
+        elif message.author.name == "ABakedFish":
+            if "fall" in message.content.lower():
+                await message.channel.send("LeTs PlAy FaLl GuYs")
+        elif message.content == "!games":
+            await message.channel.send("@everyone val or fallguys?")
+        elif message.content == "!fishtime":
+            fishtime_str = "<:FishEmote:742769011833438241> " * 10 +"\n" + "<:FishEmote:742769011833438241> " * 10
+            await message.channel.send(fishtime_str)
+            await message.channel.send(fishtime_str)
+            await message.channel.send(fishtime_str)
+            await message.channel.send(fishtime_str)
+            await message.channel.send(fishtime_str)
+        elif message.content == "!fallguys":
+            ascii_art = ("⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠉⠉⠉⠉⠉⠉⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n"
+                        "⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠄⢀⣠⣶⣶⣶⣶⣤⡀⠄⠄⠹⣿⣿⣿⣿⣿⣿⣿⣿\n"
+                        "⣿⣿⣿⣿⣿⣿⣿⣿⡏⠄⠄⣾⡿⢿⣿⣿⡿⢿⣿⡆⠄⠄⢻⣿⣿⣿⣿⣿⣿⣿\n"
+                        "⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄⠄⢿⣇⣸⣿⣿⣇⣸⡿⠃⠄⠄⠸⣿⣿⣿⣿⣿⣿⣿\n"
+                        "⣿⣿⣿⣿⣿⡿⠋⠄⠄⠄⠄⠄⠉⠛⠛⠛⠛⠉⠄⠄⠄⠄⠄⠄⠙⣿⣿⣿⣿⣿\n"
+                        "⣿⣿⣿⣿⡟⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⢿⣿⣿⣿\n"
+                        "⣿⣿⣿⡟⠄⠄⠄⠠⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⢿⣿⣿\n"
+                        "⣿⣿⡟⠄⠄⠄⢠⣆⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣧⠄⠄⠄⠈⢿⣿\n"
+                        "⣿⣿⡇⠄⠄⠄⣾⣿⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢰⣿⣧⠄⠄⠄⠘⣿\n"
+                        "⣿⣿⣇⠄⣰⣶⣿⣿⣿⣦⣀⡀⠄⠄⠄⠄⠄⠄⠄⢀⣠⣴⣿⣿⣿⣶⣆⠄⢀⣿\n"
+                        "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠄⠄⢸⣿⠇⠄⠄⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n"
+                        "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣤⣴⣾⣿⣶⣤⣤⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n")
+            await message.channel.send(ascii_art)
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    client = MyClient()
+    client.run("NzQ5MDc0OTc2MTE5NzE3OTg4.X0msbw.u93Iv8I4fJDQMFVU2KuTkjL5EnA")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
