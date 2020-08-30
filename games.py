9a2b882cce311e8cbd8b9cb8406b160a739ec72a
unknown <jacob@ajsmk.com>
2020-08-29 20:33:18 -0700

    0 to 1 in highlow

--- games.py
+++ games.py
@@ -5,7 +5,7 @@ class Games(object):
         self.credit_manager = credit_manager
 
     def high_low(self, guess):
-        pick = random.randint(0, 12)
+        pick = random.randint(1, 12)
         if guess == "high" and pick > 6:
             return True, pick
         elif guess == "low" and pick < 6:
