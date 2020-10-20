8d4152b8de22b1dac6714b02787325ab4525a1d8
unknown <jacob@ajsmk.com>
2020-10-20 00:15:47 -0700

    Mcdonalds

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import asyncio
 class GameClient(discord.Client):
     async def gold_gatherer(self):
         while True:
-            await asyncio.sleep(60)
+            await asyncio.sleep(300)
             self.manager.gather_gold_all()
             result = self.manager.save_players()
             if result:
@@ -33,6 +33,9 @@ class GameClient(discord.Client):
     async def on_message(self, message):
         print("Message from {0.author}: {0.content}".format(message))
         name = message.author.name
+        if message.content.lower() == "can i get a big mac?":
+            await message.channel.send("Breakfast Fruit & Maple Oatmeal Fruit 'N Yogurt Parfait Egg McMuffin® Egg White Delight McMuffin® Sausage McMuffin® Sausage McMuffin® with Egg Bacon, Egg & Cheese Biscuit Sausage Biscuit Sausage Biscuit with Egg Steak, Egg & Cheese Biscuit Bacon, Egg & Cheese McGriddles® Sausage McGriddles® Sausage, Egg & Cheese McGriddles® Bacon, Egg & Cheese Bagel Big Breakfast® Big Breakfast® with Hotcakes Hotcakes Hotcakes and Sausage Sausage Burrito Hash Browns Burgers Bacon Smokehouse Burger Double Bacon Smokehouse Burger Big Mac® Quarter Pounder®* with Cheese Double Quarter Pounder®* with Cheese Triple Cheeseburger Double Cheeseburger McDouble® Bacon McDouble® Cheeseburger Hamburger Chicken & Sandwiches Bacon Smokehouse Buttermilk Crispy Chicken Bacon Smokehouse Artisan Grilled Chicken 4 piece Chicken McNuggets® Buttermilk Crispy Tenders Classic Chicken Sandwich Artisan Grilled Chicken Sandwich Buttermilk Crispy Chicken Sandwich McChicken® Filet-O-Fish® Bacon Ranch Salad with Buttermilk Crispy Chicken Bacon Ranch Grilled Chicken Salad Southwest Buttermilk Crispy Chicken Salad Southwest Grilled Chicken Salad Salads Bacon Ranch Salad with Buttermilk Crispy Chicken Bacon Ranch Grilled Chicken Salad Southwest Buttermilk Crispy Chicken Salad Southwest Grilled Chicken Salad Side Salad Snacks & Sides World Famous Fries® Yoplait® GO-GURT® Low Fat Strawberry Yogurt Fruit 'N Yogurt Parfait Desserts & Shakes Chocolate Shake Strawberry Shake Vanilla Shake Vanilla Cone Hot Fudge Sundae McFlurry® with M&M'S® Candies Kiddie Cone Hot Caramel Sundae Strawberry Sundae McFlurry® with OREO® Cookies Baked Apple Pie Strawberry & Crème Pie Chocolate Chip Cookie Oatmeal Raisin Cookie Drinks MIX by Sprite™ Tropic Berry Hot Chocolate Coca-Cola® Chocolate Shake Strawberry Shake")
+
         if message.channel.name == "raiders-preview" and message.author.name != "minkowbadbot":
             if message.content[0] == "!":
                 commands = message.content.lower().split()
@@ -58,227 +61,6 @@ class GameClient(discord.Client):
                     return
                 result = pointer(name)
                 await message.channel.send(result)
-
-class MyClient(discord.Client):
-    def check_wager(self, wager, player):
-        print("in check wager")
-        print("wager: {}".format(wager))
-        if wager.isnumeric():
-            wager = int(wager)
-        else:
-            return False, "Please only wager positive, whole numbers."
-        player_credits = self.manager.check_credits(player)
-        if wager > player_credits:
-            return False, "Cannot wager more credits than you have."
-        return True, ""
-
-    def handle_commands(self, message):
-        if message.content == "!games":
-            return "@everyone val or fallguys?"
-        elif message.content == "!ned":
-            return NED
-        elif message.content == "!surprise":
-            return SURPRISE
-        elif message.content == "!fallguys":
-            ascii_art = ("```⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠉⠉⠉⠉⠉⠉⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n"
-                         "⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠄⢀⣠⣶⣶⣶⣶⣤⡀⠄⠄⠹⣿⣿⣿⣿⣿⣿⣿⣿\n"
-                         "⣿⣿⣿⣿⣿⣿⣿⣿⡏⠄⠄⣾⡿⢿⣿⣿⡿⢿⣿⡆⠄⠄⢻⣿⣿⣿⣿⣿⣿⣿\n"
-                         "⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄⠄⢿⣇⣸⣿⣿⣇⣸⡿⠃⠄⠄⠸⣿⣿⣿⣿⣿⣿⣿\n"
-                         "⣿⣿⣿⣿⣿⡿⠋⠄⠄⠄⠄⠄⠉⠛⠛⠛⠛⠉⠄⠄⠄⠄⠄⠄⠙⣿⣿⣿⣿⣿\n"
-                         "⣿⣿⣿⣿⡟⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⢿⣿⣿⣿\n"
-                         "⣿⣿⣿⡟⠄⠄⠄⠠⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⢿⣿⣿\n"
-                         "⣿⣿⡟⠄⠄⠄⢠⣆⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣧⠄⠄⠄⠈⢿⣿\n"
-                         "⣿⣿⡇⠄⠄⠄⣾⣿⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢰⣿⣧⠄⠄⠄⠘⣿\n"
-                         "⣿⣿⣇⠄⣰⣶⣿⣿⣿⣦⣀⡀⠄⠄⠄⠄⠄⠄⠄⢀⣠⣴⣿⣿⣿⣶⣆⠄⢀⣿\n"
-                         "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠄⠄⢸⣿⠇⠄⠄⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n"
-                         "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣤⣴⣾⣿⣶⣤⣤⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n```")
-            return ascii_art
-        elif message.content == "!reverse":
-            ascii_art = ("```\n"
-                         "⠐⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠂\n"
-                         "⠄⠄⣰⣾⣿⣿⣿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣆⠄⠄\n"
-                         "⠄⠄⣿⣿⣿⡿⠋⠄⡀⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠋⣉⣉⣉⡉⠙⠻⣿⣿⠄⠄\n"
-                         "⠄⠄⣿⣿⣿⣇⠔⠈⣿⣿⣿⣿⣿⡿⠛⢉⣤⣶⣾⣿⣿⣿⣿⣿⣿⣦⡀⠹⠄⠄\n"
-                         "⠄⠄⣿⣿⠃⠄⢠⣾⣿⣿⣿⠟⢁⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠄⠄\n"
-                         "⠄⠄⣿⣿⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠄⠄\n"
-                         "⠄⠄⣿⣿⣿⣿⣿⡟⠁⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄\n"
-                         "⠄⠄⣿⣿⣿⣿⠋⢠⣾⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄\n"
-                         "⠄⠄⣿⣿⡿⠁⣰⣿⣿⣿⣿⣿⣿⣿⣿⠗⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⡟⠄⠄\n"
-                         "⠄⠄⣿⡿⠁⣼⣿⣿⣿⣿⣿⣿⡿⠋⠄⠄⠄⣠⣄⢰⣿⣿⣿⣿⣿⣿⣿⠃⠄⠄\n"
-                         "⠄⠄⡿⠁⣼⣿⣿⣿⣿⣿⣿⣿⡇⠄⢀⡴⠚⢿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢠⠄⠄\n"
-                         "⠄⠄⠃⢰⣿⣿⣿⣿⣿⣿⡿⣿⣿⠴⠋⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⡟⢀⣾⠄⠄\n"
-                         "⠄⠄⢀⣿⣿⣿⣿⣿⣿⣿⠃⠈⠁⠄⠄⢀⣴⣿⣿⣿⣿⣿⣿⣿⡟⢀⣾⣿⠄⠄\n"
-                         "⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⢶⣿⣿⣿⣿⣿⣿⣿⣿⠏⢀⣾⣿⣿⠄⠄\n"
-                         "⠄⠄⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⠋⣠⣿⣿⣿⣿⠄⠄\n"
-                         "⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣼⣿⣿⣿⣿⣿⠄⠄\n"
-                         "⠄⠄⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢁⣴⣿⣿⣿⣿⣿⣿⣿⠄⠄\n"
-                         "⠄⠄⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⢁⣴⣿⣿⣿⣿⠗⠄⠄⣿⣿⠄⠄\n"
-                         "⠄⠄⣆⠈⠻⢿⣿⣿⣿⣿⣿⣿⠿⠛⣉⣤⣾⣿⣿⣿⣿⣿⣇⠠⠺⣷⣿⣿⠄⠄\n"
-                         "⠄⠄⣿⣿⣦⣄⣈⣉⣉⣉⣡⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⠉⠁⣀⣼⣿⣿⣿⠄⠄\n"
-                         "⠄⠄⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣾⣿⣿⡿⠟⠄⠄\n"
-                         "⠠⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄\n"
-                         "```")
-            return ascii_art
-        elif message.content == "!risk":
-            return "The dice hate me!"
-        elif message.content == "!tictactoe":
-            self.tic_tac_toe = not self.tic_tac_toe
-            if self.tic_tac_toe:
-                self.player1 = None
-                self.player2 = None
-                self.players_set = False
-                return "Starting tic tac toe, player1 say \"me\""
-            else:
-                return "Exiting tic tac toe"
-        elif "!give" in message.content:
-            if message.author.name != "kilbo":
-                return "Not authorized"
-            else:
-                credit_statement = message.content.split()
-                player = credit_statement[1]
-                credits = int(credit_statement[2])
-                self.manager.give_credits(player, credits)
-                if credits > 0:
-                    return "Gave {} credits to {}".format(credits, player)
-                else:
-                    return "Took {} credits from {}".format(abs(credits), player)
-        elif "!check_credits" in message.content:
-            credit_statement = message.content.split()
-            player = credit_statement[1]
-            credits = self.manager.check_credits(player)
-            if credits == 0:
-                return "{} is broke.".format(player)
-            return "{} has {} credits".format(player, credits)
-        elif "!check_balance" == message.content:
-            return "You have {} credits".format(self.manager.check_credits(message.author.name))
-        elif "!highlow" in message.content:
-            game_str = message.content.split()
-            if len(game_str) > 3:
-                return "Please use format \"!highlow guess wager\""
-            guess = game_str[1]
-            if guess not in ["even", "high", "low"]:
-                return "Guess must be one of [even, high, low]"
-            wager = game_str[2]
-            wager_check = self.check_wager(wager, message.author.name)
-            if not wager_check[0]:
-                return [1]
-            wager = int(wager)
-            win, roll = self.game_manager.high_low(guess)
-            roll_statement = "The roll was: {}\n".format(roll)
-            if not win:
-                self.manager.give_credits(message.author.name, -wager)
-                return roll_statement + "LOSER, You lost {} credits".format(wager)
-            else:
-                if guess == "even":
-                    wager = wager * 2
-                self.manager.give_credits(message.author.name, wager)
-                return roll_statement + "WINNER, You won {} credits".format(wager)
-        elif message.content == "!emojis" and message.author.name == "kilbo":
-            emojis = self.emojis
-            emoji_ids = [(str(emoji.id), emoji.name) for emoji in emojis]
-            worked = "No"
-            worked = self.manager.save_emojis(emoji_ids)
-            return worked
-        elif "!slots" in message.content:
-            game_str = message.content.split()
-            if len(game_str) != 2:
-                return "Please use format \"!slots wager\""
-            wager = game_str[1]
-            wager_check = self.check_wager(wager, message.author.name)
-            if not wager_check[0]:
-                return wager_check[1]
-            wager = int(wager)
-            game_results = self.game_manager.slots()
-            win = game_results[0]
-            if not win:
-                self.manager.give_credits(message.author.name, -wager)
-                result = game_results[1] + "\n" + "LOSER, You lost {} credits".format(wager)
-                return result
-            else:
-                wager = wager * 50
-                self.manager.give_credits(message.author.name, wager)
-                result = game_results[1] + "\n" + "WINNER, You won {} credits".format(wager)
-                return result
-        elif message.content.lower() == "!help":
-            return "Available commands:\n    !fallguys\n    !games\n    !fishtime\n    !tictactoe\n    !highlow\n    !check_credits\n    !check_balance"
-
-    async def on_ready(self):
-        self.tic_tac_toe = False
-        self.players_set = False
-        self.current_turn = None
-        self.game_client = None
-        self.player1 = None
-        self.player2 = None
-        self.turn_queue = Queue()
-        self.manager = CreditManager()
-        self.game_manager = Games(self.manager)
-        self.manager.create_table()
-        print("Logged on as {}".format(self.user))
-
-    async def on_message(self, message):
-        print("Message from {0.author}: {0.content}".format(message))
-
-        rand_int = random.randint(1, 20)
-        if message.author.name == "gmink" and rand_int == 10:
-            await message.channel.send("shut up console nerd")
-        elif message.author.name == "ABakedFish" and "ABakedFish" not in [self.player1, self.player2]:
-            fish_int = random.randint(0, 5)
-            if "fall" in message.content.lower():
-                await message.channel.send("lEtS pLaY fAlL gUyS")
-            elif "@" in message.content and fish_int % 2 == 0:
-                await message.channel.send("Fish wants to game? ITS FISHTIME!!!!")
-                await message.channel.send("!fishtime")
-        elif message.author.name == "Scetched":
-            ethan_int = random.randint(1, 20)
-            if ethan_int == 1:
-                await message.channel.send("\"This game sucks, I didn't read the rules and I don't understand what's going on\" - Ethan")
-        elif message.author.name == "kilbo":
-            kilbo_int = random.randint(1, 20)
-            if kilbo_int == 15:
-                await message.channel.send("\"I gotta pick up some wine, drive 40 minutes to the dispo\" - kilbo")
-        if message.content[0] == "!":
-            if message.content == "!fishtime":
-                fishtime_str = "<:FishEmote:742769011833438241> " * 10 + "\n" + "<:FishEmote:742769011833438241> " * 10
-                await message.channel.send(fishtime_str)
-                await message.channel.send(fishtime_str)
-                await message.channel.send(fishtime_str)
-                await message.channel.send(fishtime_str)
-                await message.channel.send(fishtime_str)
-            else:
-                await message.channel.send(self.handle_commands(message))
-        elif self.tic_tac_toe:
-            if not self.players_set:
-                if message.content == "me":
-                    if self.player1 is None:
-                        self.player1 = message.author.name
-                        await message.channel.send("Player 2 say \"me\"")
-                    elif self.player2 is None:
-                        self.player2 = message.author.name
-                        self.players_set = True
-                        self.current_turn = self.player1
-                        while not self.turn_queue.empty():
-                            self.turn_queue.get()
-                        self.turn_queue.put(self.player2)
-                        self.game_client = TicTacToe(self.player1, self.player2)
-                        await message.channel.send("Player 1 is: {}\nPlayer 2 is: {}\nPlayer 1 starts".format(self.player1, self.player2))
-                    else:
-                        await message.channel.send("Game is full")
-            else:
-                if message.author.name == self.player1 or message.author.name == self.player2:
-                    if message.author.name == self.current_turn:
-                        move = message.content
-                        if self.game_client.validate_move(move):
-                            resp = self.game_client.make_move(self.current_turn, move)
-                            if "wins" in resp:
-                                await message.channel.send(resp)
-                                self.tic_tac_toe = False
-                                return
-                            self.turn_queue.put(self.current_turn)
-                            self.current_turn = self.turn_queue.get()
-                            await message.channel.send(resp)
-                            await message.channel.send("{} it is now your turn.".format(self.current_turn))
-                    else:
-                        await message.channel.send("It is {}'s turn".format(self.current_turn))
 
 
 
